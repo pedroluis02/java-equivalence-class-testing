ec1c92ea73809f78bb9940ddca2edbc69478672a
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-11 16:30:00 -0500

    test: factorial function test

--- src/test/java/com/github/pedroluis02/equivalenceclasstesting/simple/FactorialTest.java
+++ src/test/java/com/github/pedroluis02/equivalenceclasstesting/simple/FactorialTest.java
@@ -1,5 +1,31 @@
 package com.github.pedroluis02.equivalenceclasstesting.simple;
 
-public class FactorialTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class FactorialTest {
+    // n < 0
+    @Test
+    public void testEquivalenceClassZero() {
+        assertThrows(AssertionError.class, () -> MathFunctions.factorial(-5));
+    }
+
+    // n = 0
+    @Test
+    public void testEquivalenceClassOne() {
+        assertEquals(1, MathFunctions.factorial(0));
+    }
+
+    // n > 0
+    @Test
+    public void testEquivalenceClassTwo() {
+        assertEquals(120, MathFunctions.factorial(5));
+        assertEquals(3628800, MathFunctions.factorial(10));
+    }
+
+    // n is max integer
+    @Test
+    public void testEquivalenceClassThree() {
+        assertEquals(479001600, MathFunctions.factorial(12));
+    }
 }
