d9a2543ecebea27201a7f445a3a9aae4627a9f9d
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-11 19:13:51 -0500

    test: suit of all tests

---
+++ src/test/java/com/github/pedroluis02/equivalenceclasstesting/AllPackageTests.java
@@ -0,0 +1,12 @@
+package com.github.pedroluis02.equivalenceclasstesting;
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Suit of All Tests")
+@SelectPackages("com.github.pedroluis02.equivalenceclasstesting")
+public class AllPackageTests {
+}
