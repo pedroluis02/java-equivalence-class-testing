a37d7802cb422b6c4b6bec0dd6e8acf447bbbc8b
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-11 19:37:02 -0500

    test: filter test classes by pattern

--- src/test/java/com/github/pedroluis02/equivalenceclasstesting/AllPackageTests.java
+++ src/test/java/com/github/pedroluis02/equivalenceclasstesting/AllPackageTests.java
@@ -1,6 +1,7 @@
 package com.github.pedroluis02.equivalenceclasstesting;
 
 
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -8,5 +9,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SuiteDisplayName("Suit of All Tests")
 @SelectPackages("com.github.pedroluis02.equivalenceclasstesting")
+@IncludeClassNamePatterns(".*Test")
 public class AllPackageTests {
 }
