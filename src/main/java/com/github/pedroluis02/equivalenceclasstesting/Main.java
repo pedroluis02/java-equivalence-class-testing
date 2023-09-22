503d743b549e44e5383ceda212e346d777db9f4a
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-22 05:30:13 -0500

    feat: calculate max factorial value

--- src/main/java/com/github/pedroluis02/equivalenceclasstesting/Main.java
+++ src/main/java/com/github/pedroluis02/equivalenceclasstesting/Main.java
@@ -1,7 +1,17 @@
 package com.github.pedroluis02.equivalenceclasstesting;
 
+import com.github.pedroluis02.equivalenceclasstesting.maxvalue.MaxIntegerValueFinder;
+import com.github.pedroluis02.equivalenceclasstesting.simple.MathFunctions;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java Equivalence class testing");
+        findMaxFactorialValue();
+    }
+
+    private static void findMaxFactorialValue() {
+        var finder = new MaxIntegerValueFinder(0, 30);
+        var result = finder.calculate(MathFunctions::factorial);
+        System.out.println("Last max value is: " + result);
     }
 }
